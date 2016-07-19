917aaf3715a5eb7c285071c8b65989ff292d6645
Ayrton Sparling <snowboarding96@yahoo.com>
2016-07-19 21:02:14 +0000

    Added git package download for linux, windows still in progress

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
-var os = require('os')
-var path = require('path')
+const os = require('os');
+const path = require('path');
 
-var platform = os.platform()
+const platform = os.platform();
 if (platform !== 'linux' && platform !== 'darwin' && platform !== 'win32') {
-  console.error('Unsupported platform.')
-  process.exit(1)
+  console.error('Unsupported platform.');
+  process.exit(1);
 }
 
-var arch = os.arch()
+const arch = os.arch();
 if (platform === 'darwin' && arch !== 'x64') {
-  console.error('Unsupported architecture.')
-  process.exit(1)
+  console.error('Unsupported architecture.');
+  process.exit(1);
 }
 
-var ffmpegPath = path.join(
+const ffmpegPath = path.join(
   __dirname,
   'bin',
   platform,
   arch,
   platform === 'win32' ? 'ffmpeg.exe' : 'ffmpeg'
-)
+);
+
+const ffmpegGitPath = path.join(
+  __dirname,
+  'bin',
+  platform,
+  arch,
+  platform === 'win32' ? 'ffmpeg-git.exe' : 'ffmpeg-git'
+);
 
 exports.path = ffmpegPath;
+exports.gitPath = ffmpegGitPath;
